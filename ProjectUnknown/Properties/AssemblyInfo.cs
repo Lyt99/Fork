d2e59514ec2c6caebff196b1b159e4e727d2b3f0
Tony Li <lyt1999131@163.com>
2017-02-16 21:40:53 +0800

    whatever

--- ProjectUnknown/Properties/AssemblyInfo.cs
+++ ProjectUnknown/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("ProjectUnknown")]
+[assembly: AssemblyTitle("ForkEngine")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Microsoft")]
-[assembly: AssemblyProduct("ProjectUnknown")]
-[assembly: AssemblyCopyright("Copyright © Microsoft 2017")]
+[assembly: AssemblyCompany("LeafCraft")]
+[assembly: AssemblyProduct("Fork")]
+[assembly: AssemblyCopyright("Copyright © LeafCraft 2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
